864a6c7bebeb5d8329cdcaa28868c8661da8e663
IgnacioPerez98 <nachopp98@hotmail.com>
2024-06-17 20:40:26 -0300

    optimize script

--- src/backup.rs
+++ src/backup.rs
@@ -3,6 +3,8 @@ use crate::command_executor::run_command;
 pub fn full_backup(reference :&str){
     let script = format!(r#"
     #!/bin/sh
+    db2stop force
+    db2 start database manager
     db2 terminate
     db2 deactivate db bbdd2
     
@@ -30,6 +32,7 @@ pub fn incremental_backup(reference :&str){
 
     let script =  format!(r#"
         #!/bin/sh
+
         db2 terminate
         db2 deactivate db bbdd2
     
@@ -81,20 +84,7 @@ pub fn online_backup(reference :&str){
 
     let script = format!(r#"
         #!/bin/sh
-        db2 terminate
-        db2 deactivate db bbdd2
-    
-        db2 connect to bbdd2
-        db2 quiesce database immediate force connections
-        db2 connect reset
-
         db2 "BACKUP DATABASE bbdd2 ONLINE TO {route}"
-        
-        db2 connect to bbdd2
-        db2 unquiesce database
-        db2 connect reset
-        
-        db2 activate db bbdd2
     "#, route = reference);
     let script_path = "/tmp/db2_online_backup_script.sh";
     run_command(&script, script_path);    
@@ -104,6 +94,9 @@ pub fn compressed_backup(reference :&str){
 
     let script = format!(r#"
         #!/bin/sh
+        db2stop force
+        db2 start database manager
+
         db2 terminate
         db2 deactivate db bbdd2
     
@@ -130,6 +123,9 @@ pub fn tablespaces_backup(reference :&str){
 
     let script = format!(r#"
         #!/bin/sh
+        db2stop force
+        db2 start database manager
+
         db2 terminate
         db2 deactivate db bbdd2
     
